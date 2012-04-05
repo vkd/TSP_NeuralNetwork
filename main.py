fbed28e6faa3151701255d47984588a1c0cdd0fc
vkd <vkd.castle@gmail.com>
2012-04-05 22:51:43 +0400

    Create Tkinter window

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print 'This is first program in github'
+from Tkinter import *
+import ttk
+
+def print_out():
+	print "my console"
+
+root = Tk()
+root.resizable(False, False)
+root.geometry('300x200')
+b = ttk.Button(root,
+					text = "My first buiion",
+					command = print_out)
+b.pack()
+root.mainloop()
